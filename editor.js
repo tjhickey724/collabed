2cc8c051ef451e0d709e146fe7071b58b5bfdd18
tim <tjhickey@brandeis.edu>
2019-03-16 11:13:58 -0400

    isolated string ops to separate class

--- editor.js
+++ editor.js
@@ -25,12 +25,28 @@ class TextWindow{
     this.cols = cols
   }
 
+  getNumRows(){
+    return this.rows
+  }
+
+  getNumCols(){
+    return this.cols
+  }
+
   getRowOffset(){
     return this.rowOffset
   }
 
+  setRowOffset(row){
+    this.rowOffset = row
+  }
+
   getColOffset(){
     return this.colOffset
+  }
+
+  setColOffset(col){
+    this.colOffset = col
   }
 
   getLastRow(){
@@ -61,6 +77,42 @@ class TextWindow{
     this.cursor[1]= col
   }
 
+  insertChar(row,col,key){
+    const line = this.getLine(row)
+    const first = line.substring(0,col) // first part
+    const rest = line.substring(col)
+    const newline = first+key+rest
+    this.text[row]=newline
+  }
+
+  removeChar(row,col){
+    const line = this.text[row]
+    this.text.splice(row,1,
+      line.substring(0,col-1)+line.substring(col))
+  }
+
+  joinWithNextLine(row){
+    console.log("in joinWithNexLine")
+    console.log(JSON.stringify(this.text))
+    this.text.splice(row,2,
+      this.text[row]+ this.text[row+1])
+    console.log(JSON.stringify(this.text))
+  }
+
+  splitRow(row,pos){
+    const line = this.text[row]
+    this.text.splice(row,1,
+      line.substring(0,pos),line.substring(pos))
+  }
+
+  getLine(row){
+    return this.text[row]
+  }
+
+  getCurrentLine() {
+    return this.text[this.cursor[0]]
+  }
+
   getCurrentLineLength(){
     return this.text[this.cursor[0]].length
   }
@@ -71,7 +123,7 @@ class CanvasEditor{
 
   constructor(mset,textWindow){
     this.msetCanvas = mset
-    this.textWindow = textWindow
+    this.state = textWindow
 
     this.fontColor = "black"
     this.fonttype = "32pt Courier"
@@ -98,8 +150,6 @@ class CanvasEditor{
     let numRows = Math.floor(this.msetCanvas.height/this.lineHeight);
     let numCols = Math.floor(this.msetCanvas.width/(this.charWidth))
 
-
-    this.state = new TextWindow('ddll')
     this.state.setRowsCols(numRows,numCols);
     console.log("inside the constructor!!!!")
     console.dir(this.state)
@@ -150,11 +200,12 @@ class CanvasEditor{
       this.lineDescent = this.fontSize.descent
       let numRows = Math.floor(this.msetCanvas.height/this.lineHeight);
       let numCols = Math.floor(this.msetCanvas.width/(this.charWidth))
-      this.state.cols = numCols
-      this.state.rows = numRows
+
+      // this.state.cols = numCols
+      // this.state.rows = numRows
       //let state = this.state
     //  console.dir(state)
-    //  state.setRowsCols(numRows,numCols)
+      this.state.setRowsCols(numRows,numCols)
 
       return this.ctx.measureText(this.letters);
   }
@@ -178,42 +229,56 @@ class CanvasEditor{
         // ^A beginning of line ^E end of line
         // ^D delete next character
       } else if (key=='ArrowLeft'){
-        if (state.getCurrentCol()>0){
-          // const lineLen = state.getCurrentLineLength() //state.text[state.cursor[0]].length
-          // if (state.getCurrentCol()>lineLen){
-          //   state.setCurrentCol(lineLen) //state.cursor[1] = lineLen
-          // }
-          state.setCurrentCol(state.getCurrentCol()-1)
+        let ccol = state.getCurrentCol()
+        if (ccol>0){
+          const lineLen = state.getCurrentLineLength() //state.text[state.cursor[0]].length
+          if (ccol>lineLen){
+             state.setCurrentCol(lineLen) //state.cursor[1] = lineLen
+          }
+          state.setCurrentCol(ccol-1)
           //state.cursor[1]--;
         } else {
-          if (state.getCurrentRow() >0) {
-            state.setCurrentRow(state.getCurrentRow()-1)
-            state.setCurrentCol(state.getRowLength(state.getCurrentRow()))
+          let crow = state.getCurrentRow()
+          if (crow>0) {
+            state.setCurrentRow(crow-1)
+            state.setCurrentCol(state.getRowLength(crow-1))
             // state.cursor[0]--
             // state.cursor[1] = state.text[state.cursor[0]].length
           }
         }
         return
       } else if (key=='ArrowRight'){
-        if (state.cursor[1]<state.text[state.cursor[0]].length){
-          state.cursor[1]++;
-        } else {
-          if (state.cursor[0]<state.text.length-1) {
-            state.cursor[0]++
-            state.cursor[1]=0
+          let ccol = state.getCurrentCol()
+          if (ccol< state.getCurrentLineLength()){
+             state.setCurrentCol(state.getCurrentCol()+1)
+          } else {
+             let crow = state.getCurrentRow()
+             if (crow<state.getLastRow()) {
+                state.setCursor(crow+1,0)
+             }
           }
-        }
 
         return
       } else if (key=='ArrowUp'){
-        if (state.cursor[0]>0){
-          state.cursor[0]--;
+        let crow = state.getCurrentRow()
+        if (crow>0){
+          state.setCurrentRow(crow-1)
+          let ccol = state.getCurrentCol()
+          let linelen = state.getRowLength(crow-1)
+          state.setCurrentCol(Math.min(ccol,linelen))
         }
         return
       } else if (key=='ArrowDown'){
-        if (state.cursor[0]<state.text.length-1){
-          state.cursor[0]++;
+        let crow = state.getCurrentRow()
+        let lastRow = state.getLastRow()
+
+        if (crow<lastRow){
+          state.setCurrentRow(crow+1)
+          let ccol = state.getCurrentCol()
+          let linelen = state.getRowLength(crow+1)
+          state.setCurrentCol(Math.min(ccol,linelen))
         }
+
         return
       } else if (key=='Backspace'){
         this.removePrevChar()
@@ -231,45 +296,39 @@ class CanvasEditor{
     }
 
     removePrevChar(){
-      const row = this.state.cursor[0]
-      const line = this.state.text[row]
-      const lineLen = this.state.text[row].length
-      if (this.state.cursor[1]>lineLen){
-        this.state.cursor[1] = lineLen
+      const row = this.state.getCurrentRow()
+      const col = this.state.getCurrentCol()
+      const line = this.state.getCurrentLine()
+      const lineLen = line.length
+      if (col>lineLen){
+        col = lineLen
+        this.state.setCurrentCol(col)
       }
-      const col = this.state.cursor[1]
       if (col>0){
-        this.state.text.splice(row,1,
-          line.substring(0,col-1)+line.substring(col))
-        this.state.cursor[1]--;
+        this.state.removeChar(row,col)
+        this.state.setCursor(row,col-1)
       } else if(row>0){
-        const prevLine = this.state.text[row-1]
-        this.state.text.splice(row-1,2,
-          prevLine+line)
-        this.state.cursor=[row-1,prevLine.length]
+        const prevLine = this.state.getLine(row-1)
+        this.state.joinWithNextLine(row-1)
+        this.state.setCursor(row-1,prevLine.length)
       }
-
     }
 
 
     insertCRLF(){
-      const newrow =this.state.cursor[0]
-      const pos = this.state.cursor[1]
-      const line = this.state.text[newrow]
+      const row =this.state.getCurrentRow()
+      const pos = this.state.getCurrentCol()
 
-      this.state.text.splice(newrow,1,
-        line.substring(0,pos),line.substring(pos))
-      this.state.cursor[0]++;
-      this.state.cursor[1]=0;
+      this.state.splitRow(row,pos)
+      this.state.setCursor(row+1,0)
     }
 
     insertKey(key){
-      const line = this.state.text[this.state.cursor[0]] // get current line
-      const first = line.substring(0,this.state.cursor[1]) // first part
-      const rest = line.substring(this.state.cursor[1])
-      const newline = first+key+rest
-      this.state.text[this.state.cursor[0]] = newline
-      this.state.cursor[1] += 1
+      const row = this.state.getCurrentRow()
+      const col = this.state.getCurrentCol()
+
+      this.state.insertChar(row,col,key)
+      this.state.setCursor(row,col+1)
     }
 
     redrawmsetCanvas(){
@@ -280,29 +339,34 @@ class CanvasEditor{
       //ctx.font = fonttype
       ctx.fillStyle='black'
 
-      if ((theState.cursor[0]<theState.rowOffset)  ) {
-        theState.rowOffset = theState.cursor[0]-5;
-        theState.rowOffset = Math.max(theState.rowOffset, 0);
-      } else if (theState.cursor[0]>= theState.rowOffset+theState.rows){
-        theState.rowOffset += 5;
+      if ((theState.getCurrentRow()<theState.getRowOffset())  ) {
+        theState.setRowOffset(Math.max(0,theState.getCurrentRow()-5))
+      } else if (theState.getCurrentRow() >=
+                   theState.getRowOffset()+theState.getNumRows()){
+        theState.setRowOffset(theState.getRowOffset()+5)
       }
 
-      if ((theState.cursor[1]<theState.colOffset)  ) {
-        theState.colOffset = theState.cursor[1]-5;
-        theState.colOffset = Math.max(theState.colOffset, 0);
-      } else if (theState.cursor[1]>= theState.colOffset+theState.cols){
-        theState.colOffset = theState.cursor[1]-(theState.cols-5);
-        theState.colOffset = Math.max(theState.colOffset,0)
+      let col = theState.getCurrentCol()
+      let colOffset = theState.getColOffset()
+      let numCols = theState.getNumCols()
+      if ((col<colOffset)  ) {
+        theState.setColOffset(Math.max(0,col-5))
+      } else if (col>= colOffset+numCols){
+        theState.setColOffset(Math.max(0,colOffset + numCols-5));
       }
+      colOffset = theState.getColOffset()
 
-      var rowEnd = Math.min(theState.text.length,theState.rows+theState.rowOffset)
+      let rowOffset = theState.getRowOffset()
+      let numRows = theState.getNumRows()
+      let rowEnd = Math.min(theState.getLastRow(),numRows+rowOffset)
+      numCols = theState.getNumCols()
 
 
-      for(let i=theState.rowOffset; i< rowEnd ; i++){
-        const line =theState.text[i].substring(theState.colOffset,theState.colOffset+theState.cols+5)
+      for(let i=rowOffset; i<= rowEnd ; i++){
+        const line =theState.getLine(i).substring(colOffset,colOffset+numCols+5)
         const text = ctx.measureText(line)
         const start = 0
-        const baseline = (1+i-theState.rowOffset)*this.lineHeight+this.lineDescent
+        const baseline = (1+i-rowOffset)*this.lineHeight+this.lineDescent
         const topline = this.lineHeight
         ctx.fillText(line,start,baseline)
       }
@@ -316,20 +380,20 @@ class CanvasEditor{
 
 
     drawCursor(){
-      const line =this.state.text[this.state.cursor[0]]
-      const first = line.substring(0,this.state.cursor[1])
-      const ctx = this.msetCanvas.getContext('2d')
-      ctx.fillStyle='black'
-
-
-      const visibleColumn = (first.length-this.state.colOffset)
-
-      const visibleRow = (this.state.cursor[0]-this.state.rowOffset)
-
+      const line =this.state.getCurrentLine()
+      const col = this.state.getCurrentCol()
+      const row = this.state.getCurrentRow()
+      const colOffset = this.state.getColOffset()
+      const rowOffset = this.state.getRowOffset()
+      const visibleColumn = (col-colOffset)
+      const visibleRow = (row-rowOffset)
 
       const start = visibleColumn*this.charWidth
       const baseline = visibleRow*this.lineHeight+this.lineSep+this.lineDescent
       const topline = this.lineHeight-this.lineSep+this.lineDescent+1
+
+      const ctx = this.msetCanvas.getContext('2d')
+      ctx.fillStyle='black'
       ctx.fillRect(start,baseline, 1,topline)
     }
 
